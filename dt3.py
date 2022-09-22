715aa0aa1c3f1f66ea2894e2e3609da234d0599f
xatzos21 <xatzos21@gmail.com>
2022-09-22 11:45:08 +0200

    dt

--- dt3.py
+++ dt3.py
@@ -5,38 +5,40 @@
 
 import os, datetime, time
 
-# os.system("clear")
-# x = int(input("Enter year of birth: "))
-# time.sleep(1)
-# os.system("clear")
-# y = int(input("Enter month of birth: "))
-# time.sleep(1)
-# os.system("clear")
-# z = int(input("Enter day of birth: "))
-# time.sleep(1)
-# os.system("clear")
+os.system("clear")
+x = int(input("Enter year of birth: "))
+time.sleep(1)
+os.system("clear")
+y = int(input("Enter month of birth: "))
+time.sleep(1)
+os.system("clear")
+z = int(input("Enter day of birth: "))
+time.sleep(1)
+os.system("clear")
 
-# a = datetime.date(x, y, z)
-# b = datetime.date.today()
-# print("Days passed since you were born: ", str(b - a))
+a = datetime.date(x, y, z)
+b = datetime.date.today()
+print("Days passed since you were born: ", str(b - a))
 
 
 #Task is to create an event Day Calculator
 # - takes date as input
 # - prints out years, months, weeks & days until
 
-# time.sleep(1)
-# os.system("clear")
-# X = input("Enter date to be noted in this format yyyy-mm-dd ")
-# Y = datetime.datetime.strptime(X, "%Y-%m-%d")
-# Z = datetime.datetime.today()
-# print("Days left until the given date: ", str(Y-Z))
+time.sleep(1)
+os.system("clear")
+X = input("Enter date to be noted in this format yyyy-mm-dd ")
+Y = datetime.datetime.strptime(X, "%Y-%m-%d")
+Z = datetime.datetime.today()
+print("Days left until the given date: ", str(Y-Z))
 
 
 #Personal Project Datetime!
 # - use datetime
 # - surprise us!
 
+time.sleep(1)
+os.system("clear")
 r21 = datetime.date.today()
 for i in range(0,3):
     print(r21 + datetime.timedelta(days=i))
