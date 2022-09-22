89c7d2faf68d6cb33626e9972bf5cb00a25450f8
xatzos21 <xatzos21@gmail.com>
2022-09-22 10:45:18 +0200

    dt

--- dt3.py
+++ dt3.py
@@ -3,12 +3,31 @@
 # - takes birthday as input
 # - prints out years, months, weeks & days since
 
-import os, datetime
+import os, datetime, time
 
-os.system("clear")
-a = input("Enter birthday d,m,y:")
-x = datetime.date(a)
-y = datetime.date.fromisoformat(x)
-z = datetime.date.today()
+# os.system("clear")
+# x = int(input("Enter year of birth: "))
+# time.sleep(1)
+# os.system("clear")
+# y = int(input("Enter month of birth: "))
+# time.sleep(1)
+# os.system("clear")
+# z = int(input("Enter day of birth: "))
+# time.sleep(1)
+# os.system("clear")
 
-print(x)
+# a = datetime.date(x, y, z)
+# b = datetime.date.today()
+# print("Days passed since you were born: ", str(b - a))
+
+
+#Task is to create an event Day Calculator
+# - takes date as input
+# - prints out years, months, weeks & days until
+
+# time.sleep(1)
+# os.system("clear")
+# X = input("Enter date in this format yyyy-mm-dd ")
+# Y = datetime.datetime.strptime(X, "%Y-%m-%d")
+# Z = datetime.datetime.today()
+# print("Days left until the given date: ", str(Y-Z))
